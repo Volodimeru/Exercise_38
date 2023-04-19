d0dfe8b4ad63156a2a3f2684e6087c15f78079dc
Volodimeru <126890668+Volodimeru@users.noreply.github.com>
2023-04-19 20:44:00 +0400

    Update shirtificate.py

--- shirtificate.py
+++ shirtificate.py
@@ -5,7 +5,7 @@ def main():
     pdf.add_page()
     pdf.set_font("helvetica", "B", 46)
     pdf.cell(0,40, txt="CS50 Shirtificate", align="C")
-    pdf.image("/workspaces/126890668/shirtificate.png", x=0, y=60, w=210)
+    pdf.image("shirtificate.png", x=0, y=60, w=210)
     pdf.set_font("helvetica", "B", 26)
     pdf.set_text_color(255, 255, 255)
     pdf.cell(-190,250,txt=f"{name} took CS50", align="C")
